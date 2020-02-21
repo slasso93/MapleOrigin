22e84a55036e2c7406c9585d63c64947f342ba95
slasso <slasso93@gmail.com>
2020-02-20 22:39:34 -0500

    Fix table declaration order for FK. Added unique key on inventoryitemid in inventoryequipment

--- sql/db_database.sql
+++ sql/db_database.sql
@@ -12925,6 +12925,25 @@ CREATE TABLE IF NOT EXISTS `hwidbans` (
   UNIQUE KEY `hwid_2` (`hwid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+CREATE TABLE IF NOT EXISTS `inventoryitems` (
+  `inventoryitemid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint(3) unsigned NOT NULL,
+  `characterid` int(11) DEFAULT NULL,
+  `accountid` int(11) DEFAULT NULL,
+  `itemid` int(11) NOT NULL DEFAULT '0',
+  `inventorytype` int(11) NOT NULL DEFAULT '0',
+  `position` int(11) NOT NULL DEFAULT '0',
+  `quantity` int(11) NOT NULL DEFAULT '0',
+  `owner` tinytext NOT NULL,
+  `petid` int(11) NOT NULL DEFAULT '-1',
+  `flag` int(11) NOT NULL,
+  `expiration` bigint(20) NOT NULL DEFAULT '-1',
+  `giftFrom` varchar(26) NOT NULL,
+  PRIMARY KEY (`inventoryitemid`),
+  KEY `CHARID` (`characterid`),
+  INDEX `accountid` (`accountid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 CREATE TABLE IF NOT EXISTS `inventoryequipment` (
   `inventoryequipmentid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `inventoryitemid` int(10) unsigned NOT NULL DEFAULT '0',
@@ -12951,28 +12970,10 @@ CREATE TABLE IF NOT EXISTS `inventoryequipment` (
   `itemexp` int(11) unsigned NOT NULL DEFAULT '0',
   `ringid` int(11) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`inventoryequipmentid`),
-  FOREIGN KEY (inventoryitemid)
-          REFERENCES inventoryitems (inventoryitemid)
+  UNIQUE KEY (`inventoryitemid`),
+  FOREIGN KEY (`inventoryitemid`)
+          REFERENCES inventoryitems (`inventoryitemid`)
           ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-CREATE TABLE IF NOT EXISTS `inventoryitems` (
-  `inventoryitemid` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` tinyint(3) unsigned NOT NULL,
-  `characterid` int(11) DEFAULT NULL,
-  `accountid` int(11) DEFAULT NULL,
-  `itemid` int(11) NOT NULL DEFAULT '0',
-  `inventorytype` int(11) NOT NULL DEFAULT '0',
-  `position` int(11) NOT NULL DEFAULT '0',
-  `quantity` int(11) NOT NULL DEFAULT '0',
-  `owner` tinytext NOT NULL,
-  `petid` int(11) NOT NULL DEFAULT '-1',
-  `flag` int(11) NOT NULL,
-  `expiration` bigint(20) NOT NULL DEFAULT '-1',
-  `giftFrom` varchar(26) NOT NULL,
-  PRIMARY KEY (`inventoryitemid`),
-  KEY `CHARID` (`characterid`),
-  INDEX `accountid` (`accountid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `inventorymerchant` (
