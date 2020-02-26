d213e9b74df6cafd440054e8470d528ccaf243b1
slasso <slasso93@gmail.com>
2020-02-25 20:24:13 -0500

    Fix @fm/@tele lost memories when used inside FM rooms. Fix invalid timestamp conversions

--- src/client/MapleCharacter.java
+++ src/client/MapleCharacter.java
@@ -11300,7 +11300,7 @@ public class MapleCharacter extends AbstractMapleCharacterObject {
             ps.setInt(1, accountId);
             ResultSet rs = ps.executeQuery();
             if(!rs.next()) return "Account does not exist.";
-            if(rs.getLong("tempban") != 0 && !rs.getString("tempban").equals("2018-06-20 00:00:00.0")) return "Account has been banned.";
+            if(!rs.getString("tempban").startsWith("2018-06-20 00:00")) return "Account has been banned.";
         } catch(SQLException e) {
             e.printStackTrace();
             FilePrinter.printError(FilePrinter.CHANGE_CHARACTER_NAME, e);
