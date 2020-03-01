e7eb57c03d40f4780941e354f3bb3efb4944bb02
slasso <slasso93@gmail.com>
2020-02-29 20:36:01 -0500

    Fix potential dupe exploit

--- src/net/server/channel/handlers/InventoryMergeHandler.java
+++ src/net/server/channel/handlers/InventoryMergeHandler.java
@@ -52,61 +52,63 @@ public final class InventoryMergeHandler extends AbstractMaplePacketHandler {
             c.disconnect(false, false);
             return;
         }
-        
-        MapleInventoryType inventoryType = MapleInventoryType.getByType(invType);	
-	MapleInventory inventory = c.getPlayer().getInventory(inventoryType);
-        inventory.lockInventory();
-        try {
-            //------------------- RonanLana's SLOT MERGER -----------------
-        
-            MapleItemInformationProvider ii = MapleItemInformationProvider.getInstance();
-            Item srcItem, dstItem;
+        if (c.tryacquireClient()) {
+            MapleInventoryType inventoryType = MapleInventoryType.getByType(invType);
+            MapleInventory inventory = c.getPlayer().getInventory(inventoryType);
+            inventory.lockInventory();
+            try {
+                //------------------- RonanLana's SLOT MERGER -----------------
 
-            for(short dst = 1; dst <= inventory.getSlotLimit(); dst++) {
-                dstItem = inventory.getItem(dst);
-                if(dstItem == null) continue;
+                MapleItemInformationProvider ii = MapleItemInformationProvider.getInstance();
+                Item srcItem, dstItem;
 
-                for(short src = (short)(dst + 1); src <= inventory.getSlotLimit(); src++) {
-                    srcItem = inventory.getItem(src);
-                    if(srcItem == null) continue;
+                for (short dst = 1; dst <= inventory.getSlotLimit(); dst++) {
+                    dstItem = inventory.getItem(dst);
+                    if (dstItem == null) continue;
 
-                    if(dstItem.getItemId() != srcItem.getItemId()) continue;
-                    if(dstItem.getQuantity() == ii.getSlotMax(c, inventory.getItem(dst).getItemId())) break;
+                    for (short src = (short) (dst + 1); src <= inventory.getSlotLimit(); src++) {
+                        srcItem = inventory.getItem(src);
+                        if (srcItem == null) continue;
 
-                    MapleInventoryManipulator.move(c, inventoryType, src, dst);
-                }
-            }
+                        if (dstItem.getItemId() != srcItem.getItemId()) continue;
+                        if (dstItem.getQuantity() == ii.getSlotMax(c, inventory.getItem(dst).getItemId())) break;
 
-            //------------------------------------------------------------
-
-            inventory = c.getPlayer().getInventory(inventoryType);
-            boolean sorted = false;
-
-            while (!sorted) {
-                short freeSlot = inventory.getNextFreeSlot();
-
-                if (freeSlot != -1) {
-                    short itemSlot = -1;
-                    for (short i = (short) (freeSlot + 1); i <= inventory.getSlotLimit(); i = (short) (i + 1)) {
-                        if (inventory.getItem(i) != null) {
-                            itemSlot = i;
-                            break;
-                        }
+                        MapleInventoryManipulator.move(c, inventoryType, src, dst);
                     }
-                    if (itemSlot > 0) {
-                        MapleInventoryManipulator.move(c, inventoryType, itemSlot, freeSlot);
+                }
+
+                //------------------------------------------------------------
+
+                inventory = c.getPlayer().getInventory(inventoryType);
+                boolean sorted = false;
+
+                while (!sorted) {
+                    short freeSlot = inventory.getNextFreeSlot();
+
+                    if (freeSlot != -1) {
+                        short itemSlot = -1;
+                        for (short i = (short) (freeSlot + 1); i <= inventory.getSlotLimit(); i = (short) (i + 1)) {
+                            if (inventory.getItem(i) != null) {
+                                itemSlot = i;
+                                break;
+                            }
+                        }
+                        if (itemSlot > 0) {
+                            MapleInventoryManipulator.move(c, inventoryType, itemSlot, freeSlot);
+                        } else {
+                            sorted = true;
+                        }
                     } else {
                         sorted = true;
                     }
-                } else {
-                    sorted = true;
                 }
+            } finally {
+                inventory.unlockInventory();
+                c.releaseClient();
             }
-        } finally {
-            inventory.unlockInventory();
+
+            c.announce(MaplePacketCreator.finishedSort(inventoryType.getType()));
+            c.announce(MaplePacketCreator.enableActions());
         }
-        
-        c.announce(MaplePacketCreator.finishedSort(inventoryType.getType()));
-        c.announce(MaplePacketCreator.enableActions());
     }
 }
