4656782b6ee5ecf83467f691bae9f2c4b4a53d94
lightryuzaki <32381002+lightryuzaki@users.noreply.github.com>
2020-04-12 14:46:44 -0700

    comp + cwk channel added

--- scripts/npc/2141002.js
+++ scripts/npc/2141002.js
@@ -1,53 +1,63 @@
 /*
-	NPC Name: 		The Forgotten Temple Manager
-	Map(s): 		Deep in the Shrine - Twilight of the gods
-	Description: 		Pink Bean
+	This file is part of the OdinMS Maple Story Server
+    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
+		       Matthias Butz <matze@odinms.de>
+		       Jan Christian Meyer <vimes@odinms.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/* Amon
+ * 
+ * @Author Stereo
+ * Adobis's Mission I : Breath of Lava <Level 1> (280020000)
+ * Adobis's Mission I : Breath of Lava <Level 2> (280020001)
+ * Last Mission : Zakum's Altar (280030000)
+ * Zakum Quest NPC 
+ * Helps players leave the map
  */
-
-var status;
-
+ importPackage(Packages.server.expeditions);
+ 
 function start() {
-    status = -1;
-    action(1, 0, 0);
+    if(cm.getMapId() == 270050100) {
+        if(!cm.getEventInstance().isEventCleared()) cm.sendYesNo("If you leave now, you'll have to start over. Are you sure you want to leave?");
+        else cm.sendYesNo("Pink Bean has been defeated! You guys sure are true heroes of this land! In no time, Temple of Time will shine again as bright as ever, all thanks to your efforts! Hooray to our heroes!! Are you ready to go now?");
+		
+    } else {
+        cm.sendYesNo("If you leave now, you'll have to start over. Are you sure you want to leave?");
+    }
 }
 
 function action(mode, type, selection) {
-    if (mode == -1)
+    if (mode < 1)
         cm.dispose();
+        
+    else if(!cm.getEventInstance().isEventCleared())
+{  
+        cm.warp(211042300);
+        //cm.gainItem(4000313, 1);
+        cm.dispose();
+}
     else {
-        if (mode == 0) {
+        if(!cm.getPlayer().getInventory(Packages.client.inventory.MapleInventoryType.ETC).isFull(1)){
+            cm.warp(270050000);
+            cm.gainItem(4000313, 5);
             cm.dispose();
-            return;
-        }
-        if (mode == 1)
-            status++;
-        else
-            status--;
-        
-        var eim = cm.getEventInstance();
-        if(!eim.isEventCleared()) {
-            if (status == 0) {
-                cm.sendYesNo("Do you want to get out now?");
-            }
-            else if (status == 1) {
-                cm.warp(270050000, 0);
-                cm.dispose();
-            }
-        
         } else {
-            if (status == 0) {
-                cm.sendYesNo("Pink Bean has been defeated! You guys sure are true heroes of this land! In no time, Temple of Time will shine again as bright as ever, all thanks to your efforts! Hooray to our heroes!! Are you ready to go now?");
-            }
-            else if (status == 1) {
-                if(eim.giveEventReward(cm.getPlayer(), 1)) {
-                    cm.warp(270050000);
-                }
-                else {
-                    cm.sendOk("You cannot receive an instance prize without having an empty room in your EQUIP, USE, SET-UP and ETC inventory.");
-                }
-                
-                cm.dispose();
-            }
+            cm.sendOk("Please make space in your inventory");
+            cm.dispose();
         }
     }
 }
