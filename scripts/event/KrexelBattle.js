6d8df41fa6ac4dbe9c75676958c7bcd3d4347274
slasso <slasso93@gmail.com>
2020-07-26 21:04:03 -0400

    Add krexel battle, fix mini dungeon and all portals in Ulu

---
+++ scripts/event/KrexelBattle.js
@@ -0,0 +1,228 @@
+/**
+ * @author: slasso
+ * @event: Krexel Battle
+*/
+
+importPackage(Packages.server.life);
+importPackage(Packages.server.expeditions);
+
+var isPq = true;
+var minPlayers = 1, maxPlayers = 6;
+var minLevel = 120, maxLevel = 255;
+var entryMap = 541020800;
+var exitMap = 541020700;
+var recruitMap = 541020700;
+
+var minMapId = 541020800;
+var maxMapId = 541020800;
+
+var eventTime = 60; //
+
+var lobbyRange = [0, 0];
+
+function init() {
+    setEventRequirements();
+}
+
+function setLobbyRange() {
+    return lobbyRange;
+}
+
+function setEventRequirements() {
+        var reqStr = "";
+
+        reqStr += "\r\n    Number of players: ";
+        if(maxPlayers - minPlayers >= 1) reqStr += minPlayers + " ~ " + maxPlayers;
+        else reqStr += minPlayers;
+
+        reqStr += "\r\n    Level range: ";
+        if(maxLevel - minLevel >= 1) reqStr += minLevel + " ~ " + maxLevel;
+        else reqStr += minLevel;
+
+        reqStr += "\r\n    Time limit: ";
+        reqStr += eventTime + " minutes";
+
+        em.setProperty("party", reqStr);
+}
+
+function getEligibleParty(party) {      //selects, from the given party, the team that is allowed to attempt this event
+    var eligible = [];
+    var hasLeader = false;
+
+    if(party.size() > 0) {
+        var partyList = party.toArray();
+
+        for(var i = 0; i < party.size(); i++) {
+            var ch = partyList[i];
+
+            if(ch.getMapId() == recruitMap && ch.getLevel() >= minLevel && ch.getLevel() <= maxLevel) {
+				if (ch.getPlayer().getQuestStatus(7103) == 2) {
+					eligible.push(ch);
+                } else {
+					eligible = [];
+					break;
+				}
+
+                if(ch.isLeader())
+					hasLeader = true;
+            }
+        }
+    }
+
+    if(!(hasLeader && eligible.length >= minPlayers && eligible.length <= maxPlayers)) eligible = [];
+    return eligible;
+}
+
+function setEventExclusives(eim) {
+        var itemSet = [];
+        eim.setExclusiveItems(itemSet);
+}
+
+function setEventRewards(eim) {
+        var itemSet, itemQty, evLevel, expStages, mesoStages;
+
+        evLevel = 1;    //Rewards at clear PQ
+        itemSet = [];
+        itemQty = [];
+        eim.setEventRewards(evLevel, itemSet, itemQty);
+
+        expStages = [];    //bonus exp given on CLEAR stage signal
+        eim.setEventClearStageExp(expStages);
+
+        mesoStages = [];    //bonus meso given on CLEAR stage signal
+        eim.setEventClearStageMeso(mesoStages);
+}
+
+function afterSetup(eim) {
+    updateGateState(1);
+}
+
+function setup(channel) {
+    var eim = em.newInstance("Krexel" + channel);
+    eim.setProperty("canJoin", 1);
+    eim.setProperty("defeatedBoss", 0);
+
+    var level = 1;
+    eim.getInstanceMap(entryMap).resetPQ(level);
+
+    eim.startEventTimer(eventTime * 60000);
+    setEventRewards(eim);
+    setEventExclusives(eim);
+
+    return eim;
+}
+
+function playerEntry(eim, player) {
+    var map = eim.getMapInstance(entryMap);
+    player.changeMap(map, map.getPortal(0));
+}
+
+function scheduledTimeout(eim) {
+    end(eim);
+}
+
+function changedMap(eim, player, mapid) {
+    if (mapid < minMapId || mapid > maxMapId) {
+	if (eim.isExpeditionTeamLackingNow(true, minPlayers, player)) {
+            eim.unregisterPlayer(player);
+            eim.dropMessage(5, "[Expedition] Either the leader has quit the expedition or there is no longer the minimum number of members required to continue it.");
+            end(eim);
+        }
+        else {
+            //eim.dropMessage(5, "[Expedition] " + player.getName() + " has left the instance.");
+            eim.unregisterPlayer(player);
+        }
+    }
+}
+
+function changedLeader(eim, leader) {}
+
+function playerDead(eim, player) {}
+
+function playerRevive(eim, player) {
+    if (eim.isExpeditionTeamLackingNow(true, minPlayers, player)) {
+        eim.unregisterPlayer(player);
+        eim.dropMessage(5, "[Expedition] Either the leader has quit the expedition or there is no longer the minimum number of members required to continue it.");
+        end(eim);
+    }
+    else {
+        //eim.dropMessage(5, "[Expedition] " + player.getName() + " has left the instance.");
+        eim.unregisterPlayer(player);
+    }
+}
+
+function playerDisconnected(eim, player) {
+    if (eim.isExpeditionTeamLackingNow(true, minPlayers, player)) {
+        eim.unregisterPlayer(player);
+        eim.dropMessage(5, "[Expedition] Either the leader has quit the expedition or there is no longer the minimum number of members required to continue it.");
+        end(eim);
+    }
+    else {
+        //eim.dropMessage(5, "[Expedition] " + player.getName() + " has left the instance.");
+        eim.unregisterPlayer(player);
+    }
+}
+
+function leftParty (eim, player) {}
+
+function disbandParty (eim) {}
+
+function monsterValue(eim, mobId) {
+    return 1;
+}
+
+function playerUnregistered(eim, player) {
+    if (eim.isEventCleared()) { // complete any quest if needed
+        // em.completeQuest(player, 100200, 2030010);
+    }
+}
+
+function playerExit(eim, player) {
+    eim.unregisterPlayer(player);
+    player.changeMap(exitMap, "boss00");
+}
+
+function end(eim) {
+    var party = eim.getPlayers();
+    for (var i = 0; i < party.size(); i++) {
+        playerExit(eim, party.get(i));
+    }
+    eim.dispose();
+}
+
+
+function clearPQ(eim) {
+    eim.stopEventTimer();
+    eim.setEventCleared(MapleExpeditionType.KREXEL);
+    updateGateState(0);
+}
+
+function isKrexel(mob) {
+    var mobid = mob.getId();
+    return (mobid == 9420522);
+}
+
+function monsterKilled(mob, eim) {
+    if (isKrexel(mob)) {
+        eim.setIntProperty("defeatedBoss", 1);
+        eim.showClearEffect(mob.getMap().getId());
+        eim.clearPQ();
+
+        mob.getMap().broadcastKrexelVictory();
+    }
+}
+
+function giveRandomEventReward(eim, player) {
+    eim.giveEventReward(player);
+}
+
+function allMonstersDead(eim) {}
+
+function cancelSchedule() {}
+
+function updateGateState(newState) {
+}
+
+function dispose(eim) {
+
+}
