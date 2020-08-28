5a9c2642f9c1dd8e66aef86f854b3fe0ccd45dcb
lightryuzaki <32381002+lightryuzaki@users.noreply.github.com>
2020-08-28 14:00:23 -0700

    Fix some NPCs

--- scripts/npc/1200002.js
+++ scripts/npc/1200002.js
@@ -3,20 +3,25 @@
   @author slasso
 */
 
-var status = 0;
+importPackage(Packages.tools);
+importPackage(Packages.constants.inventory);
+importPackage(Packages.client.inventory);
+
+var status;
 
 var rings = [1112135, 1115124, 1112196, 1112195, 1112161, 1112198, 1112165, 1112171, 1115108, 1112238, 1115035, 1115009, 1115008, 1112273, 1115011, 1112277, 1112283, 1115019];
-var wings = [1102758, 1102378, 1102376, 1102624, 1102487, 1102184, 1102252, 1102253, 1102273, 1102318, 1102338, 1102349, 1102532, 1102546, 1102547, 1102548, 1102554, 1102604, 
-				1102630, 1102642, 1102668, 1102697, 1102698, 1102699, 1102723, 1102724, 1102729, 1102779, 1102798, 1102806, 1102823, 1102824, 1102874, 1102377, 
+var wings = [1102758, 1102378, 1102376, 1102624, 1102487, 1102184, 1102252, 1102253, 1102318, 1102338, 1102349, 1102532, 1102546, 1102547, 1102548, 1102554, 1102604, 
+				1102630, 1102642, 1102668, 1102697, 1102698, 1102699, 1102729, 1102779, 1102798, 1102806, 1102823, 1102824, 1102874, 1102377, 
 				1102385, 1102386, 1102450, 1102451, 1102452, 1102453];
 var pets = [5000060, 5000111, 5000146, 5000176, 5000200, 5000243, 5000261, 5000270, 5000271, 5000272, 5000273, 5000293, 5000294, 5000295, 5000317, 5000621, 5000275];
 
 var fhair = Array(37450, 38940, 38680, 38860, 39190, 39410, 39230, 39040);
 var mhair = Array(38340, 40650, 40060, 40300, 37160, 40390, 40400, 40510);
-var mface = new Array(23086, 24012, 25025, 25011, 21078, 25050);
-var fface = new Array(25044, 24079, 26096, 28020, 27017, 27053, 21094);
+var mface = new Array(23086, 24012, 25025, 25011, 21078, 25050,23029, 23033, 23040, 23057, 23055, 23069, 23073, 23072, 23084, 23079, 24001, 25013, 25014, 25015, 25018, 25019, 25020, 25022, 25024, 25027, 25031, 25056, 25039, 25079);
+var fface = new Array(25044, 24079, 26096, 28020, 27017, 27053, 21094, 21077, 21076, 21070, 21084, 21081, 21079, 24005, 24008, 24010, 24012, 24013, 24015, 24015, 24022, 24029, 24028, 24027, 24020, 24037, 24041, 24055, 24060, 24064, 24072, 24081, 24082, 24093, 24092, 24098, 25006, 25038, 25083, 26028);
 var beauty = -1;
 
+var item;
 var type;
 var costs = {
 	rings: 2,
@@ -28,12 +33,13 @@ var costs = {
 };
 
 function start() {
+	status=-1;
     cm.sendSimple("Hi! I can exchange #v4000492# for various donor items! What would u like to buy? (More to come!)#b\r\n" + 
 			"#L1# Buy Chat & Label Rings for 2 CoGT  #b\r\n" + 
 			"#L2# Buy Pets for 5 CoGT#b\r\n" + 
 			"#L3# Name Change for 10 CoGT#b\r\n" + 
 			"#L4# Buy Wings for 10 CoGT\r\n" + 
-			"#L5# Haircut for 5 CoGT \r\n" +
+			"#L5# Haircut for 5 CoGT [BUG: Choose hair color before haircut. Working on fix. \r\n" +
 			"#L6# Plastic Surgery for 5 CoGT"
 	);
 }
@@ -45,7 +51,7 @@ function action (m,t,s) {
     } else {
         status++;
     }
-    if (status == 1) {
+    if (status == 0) {
         if (s == 1) {
 			type = 'rings';
             var selStr = "#e#kRing Shop:#n #r2#b #e#z4000492##n#k\r\n#bRings are +1 all stats#b";
@@ -93,7 +99,16 @@ function action (m,t,s) {
             }
             cm.sendStyle("#eWant Plastic Surgery? If you have a #b#t4000492##k I'll change it for you!", facenew);
         }
-    } else if (status == 2) {
+		if (!type) status++; // skip confirmation dialogue for non item purchase
+    } else if(status == 1) {
+		if (type) {
+			if (type == 'rings') item = rings[s];
+			if (type == 'pets') item = pets[s];
+			if (type == 'wings') item = wings[s];
+			
+			cm.sendYesNo("Are you sure you want to buy #e#b#z" + item + "##n#k for #r" + costs[type] + " #b#e#z4000492##n#k?");
+		}
+	} else if (status == 2) {
         var text = cm.getText();
 
         if (beauty == 1) { // hair
@@ -125,16 +140,16 @@ function action (m,t,s) {
                 cm.sendOk("#e#rYou'll need " + costs.name + " #b#t4000492##k");
 			}
         } else { // item purchase
-			if (cm.haveItem(4000492, costs[type])) {
-				cm.gainItem(4000492, -costs[type]);
-				if (type == 'rings') cm.gainItem(rings[s], 1);
-				if (type == 'pets') cm.gainItem(pets[s], 1, false, true, 10 * 365 * 1000 * 60 * 60 * 24); // 10 years before it dries up
-				if (type == 'wings') cm.gainItem(wings[s], 1);
-				
-			} else {
+			if (!cm.haveItem(4000492, costs[type])) {
                 cm.sendOk("#e#rYou'll need " + costs[type] + " #b#t4000492##k");
+            } else if (cm.getPlayer().getInventory(ItemConstants.getInventoryType(item)).isFull(0)) {
+                cm.sendOk("Your inventory is full! Please make room and try again.");
+            } else {
+				cm.gainItem(4000492, -costs[type]);
+				if (type == 'pets') cm.gainItem(item, 1, false, true, 10 * 365 * 1000 * 60 * 60 * 24); // 10 years before it dries up
+				else cm.gainItem(item, 1);
+				
 			}
-			
 		}
 		cm.dispose();
 	}
--- scripts/npc/2002000.js
+++ scripts/npc/2002000.js
@@ -1,54 +1,12 @@
-/*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
+var common = [1702337, 1702340, 1702347, 1702427, 1702428, 1702479, 1702523, 1702550, 1702554, 1702564, 1702571, 1702576, 1702600, 1702606, 1702639, 1702647, 1702687, 1702698, 1702776, 1702780, 1702826, 1702845, 1702906, 
+				1702352, 1702366, 1702427, 1702461, 1702485,1702503, 1702488, 1702526, 1702534, 1702541];
+var uncommon = [1702350, 1702363, 1702366, 1702398, 1702453, 1702461, 1702488, 1702507, 1702525, 1702533, 1702559, 1702560, 1702589, 1702620, 1702629, 1702651, 1702753, 1702765, 1702769, 1702813, 1702560, 1702565, 1702571, 
+				1702589, 1702616,1702377, 1702680, 1702793, 1702766, 1702840, 1702767]; 
+var rare = [1702342, 1702360, 1702364, 1702367, 1702368, 1702379, 1702556, 1702617, 1702625, 1702633, 1702681, 1702697, 1702727, 1702752, 1702787, 1702865, 1702876, 1702927, 1702639, 1702671, 1702676, 1702718, 1702719, 1702706, 
+				1702728, 1702746, 1702733, 1702795, 1702876, 1702557];
+var urare = [1702748, 1702745, 1702753, 1702917, 1702724, 1702581, 1702773, 1702887, 1702627, 1702672, 1702771, 1702358, 1702401, 1702408, 1702415, 1702456, 1702505, 1702565, 1702585, 1702593, 1702594, 1702675, 1702770, 1702784, 1702814, 1092064];
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/**
-Rupi- Happyville Warp NPC
-**/
-/*
-function start() {
-    cm.sendYesNo("Do you want to get out of Happyville?");
-}
-
-function action(mode, type, selection) {
-    if (mode < 1)
-        cm.dispose();
-    else {
-        var map = cm.getPlayer().getSavedLocation("HAPPYVILLE");
-        if (map == -1)
-                map = 101000000;
-        
-        cm.warp(map, 0);
-    }
-    
-    cm.dispose();
-}
-*/
-
-var item;
-var common = Array(1702352, 1702366, 1702427, 1702461, 1702485,1702503, 1702488, 1702526, 1702534, 1702541);
-var normal = Array(1702560, 1702565, 1702571, 1702589, 1702616,1702377, 1702680, 1702793, 1702766, 1702840, 1702767); 
-var rare = Array(1702639, 1702671, 1702676, 1702718, 1702719, 1702706, 1702728, 1702746, 1702733, 1702795, 1702876, 1702557  );
-var rare1 = Array(1702748, 1702745, 1702753, 1702917, 1702724, 1702581);
-var rare2 = Array(1702773, 1702887, 1702627, 1702672, 1702771);
 
 function getRandom(min, max) {
     if (min > max) {
@@ -62,73 +20,52 @@ function getRandom(min, max) {
     return(min + parseInt(Math.random() * (max - min + 1)));
 }
 
-var icommon = common[getRandom(0, common.length - 1)];
-var inormal = normal[getRandom(0, normal.length - 1)];
-var irare = rare[getRandom(0, rare.length - 1)];
-var irare1 = rare1[getRandom(0, rare1.length - 1)];
-var irare2 = rare2[getRandom(0, rare2.length - 1)];
-
-var chance = getRandom(0, 9);
-
 function start() {
     status = -1;
     action(1, 0, 0);
 }
 
 function action(mode, type, selection) {
-    if (mode == -1) {
-        cm.dispose();
-    } else {
-        if (mode == 0) {
-            cm.sendOk("#rOkay, come back when you're ready to test your #eRNG!");
-            cm.dispose();
-            return;
-        } else if (mode == 1) {
-            status++;
-        }
+    if (mode == -1)
+		cm.dispose();
+	else {
+		if (mode == 0 && status == 0)
+			cm.dispose();
 
-        if (status == 0) {
-            cm.sendNext(" #i3991013##i3991023##i3991006##i3991000##i3991002##i3991007##i3991000##i3991015##i3991014##i3991013# \r\nHello #h #,\r\n\r\nWant to try your luck at the #r#eNX Gachapon?#n#k You can earn assorted up to date NX Weapons! \r\nRemember that each spin will cost you a\r\n\r\n#i05220020##t05220020#");
-        } else if (status == 1) {
-            if(!cm.getPlayer().getInventory(Packages.client.inventory.MapleInventoryType.EQUIP).isFull(2)){
-                if(cm.haveItem(5220020,1)) {
-                    if (chance > 0 && chance <= 1) {
-                            cm.sendOk("#b#eCongratulations!#n#k You have obtained a #b#t" + cm.gainItem(icommon, 1) + "##k #v" + icommon + "#");
-                             cm.gainItem(5220020, -1);
-							cm.dispose(); 
-                        
-                        } else if (chance >= 2 && chance <= 3) {
-                            cm.sendOk("#b#eCongratulations!#n#k You have obtained a #b#t" + cm.gainItem(inormal, 1) + "##k #v" + inormal + "#");
-                             cm.gainItem(5220020, -1);
-							cm.dispose();
-                        
-                        } else if (chance >=4 && chance <=5) {
-                            cm.sendOk("#b#eCongratulations!#n#k You have obtained a #d#t" + cm.gainItem(irare, 1) + "##k #v" + irare + "#");
-                             cm.gainItem(5220020, -1);
-							cm.dispose();
-                        
-                        } else if (chance >= 6 && chance <= 7) {
-                            cm.sendOk("#b#eCongratulations!#n#k You have obtained a #b#t" + cm.gainItem(irare1, 1) + "##k #v" + irare1 + "#");
-                             cm.gainItem(5220020, -1);
-							cm.dispose();
-                        
-                        } else if (chance >= 8 && chance <= 9) {
-                            cm.sendOk("#b#eCongratulations!#n#k You have obtained a #b#t" + cm.gainItem(irare2, 1) + "##k #v" + irare2 + "#");
-                            cm.gainItem(5220020, -1);
-						   cm.dispose();
-                        
-                        } else {
-                            cm.sendOk("Sorry you do not have a gachapon ticket");
-                            cm.dispose();
-                        }
-                } else {
-                    cm.sendOk("Sorry you do not have a gachapon ticket");
-                    cm.dispose();               
-                    }
+		if (mode == 1)
+			status++;
+		else
+			status--;
+
+        
+		if (status == 0) { // confirm selection with yes/no
+			cm.sendYesNo("#i3991013##i3991023##i3991006##i3991000##i3991002##i3991007##i3991000##i3991015##i3991014##i3991013#\r\n" + 
+						"Hello #h #!\r\n\r\nWant to try your luck at the #r#eNX Gachapon?#n#k You can earn assorted up to date NX WEAPONS! \r\nRemember that each spin will cost you a\r\n\r\n#i5220020##t5220020#");
+        } else if (status == 1) { // finalize the purchase
+		    var vp = cm.getPlayer().getClient().getVotePoints();
+            if (!cm.haveItem(5220020, 1)) {
+				cm.sendOk("Sorry you do not have an #i5220020##e#b#z5220020##n#k! #i5220020##e#b#z5220020##k#n can be purchased from Mia in the FM.");
+            } else if (cm.getPlayer().getInventory(Packages.client.inventory.MapleInventoryType.EQUIP).isFull(0)) {
+                cm.sendOk("Your inventory is full! Please make room and try again.");
             } else {
-                cm.sendOk("Sorry your inventory is full");
-                cm.dispose();
+				var roll = getRandom(0, 10); // random number from 0-10 to decide which array to choose from
+				var item = -1;
+				if (roll >= 0 && roll <= 4)
+					item = common[getRandom(0, common.length - 1)];
+				else if (roll >= 5 && roll <= 7)
+					item = uncommon[getRandom(0, uncommon.length - 1)];
+				else if (roll >= 8 && roll <= 9)
+					item = rare[getRandom(0, rare.length - 1)];
+				else if (roll == 10)
+					item = urare[getRandom(0, urare.length - 1)];
+				
+				if (item != -1) {					
+					cm.gainItem(5220020, -1);
+					cm.gainItem(item, 1);
+					cm.sendOk("#b#eCongratulations!#n#k You have obtained a #e#b#v" + item + "##z" + item + "#");
+				}
             }
+            cm.dispose();
         }
-    }
+	}
 }
